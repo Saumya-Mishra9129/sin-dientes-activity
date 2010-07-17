aaf31c607a4c5e4b79acbb96dd480a242c190949
Joel Davila <6336737@gmail.com>
2010-07-17 09:31:24 -0600

    reparando multiline

--- ahorcado.py
+++ ahorcado.py
@@ -34,6 +34,7 @@ class Ahorcado:
         #interface
         self.imagen = gtk.Image()
         self.instrucciones_label = gtk.Label(_('Instrucciones'))
+        self.instrucciones_label.set_justify(gtk.JUSTIFY_FILL)
         self.aciertos_label = gtk.Label('Puntaje: 0')
         self.errores_label = gtk.Label()
         self.palabra_label = gtk.Label()
