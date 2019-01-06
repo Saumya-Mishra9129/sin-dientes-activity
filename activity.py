93bac788c6d267dff2aef7aa37fb8aa8ec0f97c8
Aniket21mathur <amathur@ec.iitr.ac.in>
2019-01-06 21:32:30 +0530

    Adding copyright or licence
    The source code of the activity did not had copyright
    or licence.
    
    Now adding licence to the source code.Files changed:
    -activity.py
    -setup.py
    -utils.py
    
    Fixes https://github.com/sugarlabs/sin-dientes-activity/issues/5

--- activity.py
+++ activity.py
@@ -1,4 +1,25 @@
 # -*- coding: UTF-8 -*-
+
+# Sin-dientes-activity
+# Copyright (C) 2012, Yader Velásquez
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information:
+# Yader Velásquez <yajosev@gmail.com>
+
+
 from sugar3.activity import activity, widgets
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import ActivityToolbarButton
